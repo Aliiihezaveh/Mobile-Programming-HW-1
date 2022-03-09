8175767e222d73bf7cc59af526d7f63ad09178e6
Aliiihezaveh <ahezaveh554@gmail.com>
2022-03-09 17:39:08 +0330

    add student fragment

---
+++ MP-HW1/app/src/main/java/com/example/quera/HomeActivity.java
@@ -0,0 +1,29 @@
+package com.example.quera;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginIntent = new Intent(HomeActivity.this, Login_Activity.class);
+                startActivity(loginIntent);
+                finish();
+            }
+        });
+    }
+}
