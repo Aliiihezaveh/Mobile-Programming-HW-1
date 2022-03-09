fe04b7cf41a65264678197635cdebda925bf4f49
Aliiihezaveh <ahezaveh554@gmail.com>
2022-03-09 16:13:37 +0330

    add Splash Screen

--- MP-HW1/app/src/main/java/com/example/quera/MainActivity.java
+++ MP-HW1/app/src/main/java/com/example/quera/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.quera;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent loginIntent = new Intent(MainActivity.this, Login_Activity.class);
+                startActivity(loginIntent);
+                finish();
+            }
+        }, 3000);
     }
 }
