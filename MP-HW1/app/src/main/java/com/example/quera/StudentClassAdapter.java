e284be3068fab5fdcb0bb51ff3091cfcd1703020
Aliiihezaveh <ahezaveh554@gmail.com>
2022-03-17 12:54:54 +0330

    complete Student Class Adapter

--- MP-HW1/app/src/main/java/com/example/quera/StudentClassAdapter.java
+++ MP-HW1/app/src/main/java/com/example/quera/StudentClassAdapter.java
@@ -1,8 +1,13 @@
 package com.example.quera;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,18 +17,29 @@ import java.util.ArrayList;
 public class StudentClassAdapter extends RecyclerView.Adapter<StudentClassAdapter.StudentViewHolder> {
 
     private ArrayList<StudentClass> studentClasses = new ArrayList<>();
-    public StudentClassAdapter() {
+    private Context context;
+
+    public StudentClassAdapter(Context context) {
+        this.context = context;
     }
 
     @NonNull
     @Override
     public StudentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.class_list_item, parent, false);
+        StudentViewHolder holder = new StudentViewHolder(view);
+        return holder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull StudentViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull StudentViewHolder holder, @SuppressLint("RecyclerView") final int position) {
+        holder.studentClassName.setText(studentClasses.get(position).getClassName());
+        holder.parent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context,studentClasses.get(position).getClassName() + " Selected",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -38,9 +54,11 @@ public class StudentClassAdapter extends RecyclerView.Adapter<StudentClassAdapte
 
     public class StudentViewHolder extends RecyclerView.ViewHolder{
         private TextView studentClassName;
+        private RelativeLayout parent;
         public StudentViewHolder(@NonNull View itemView) {
             super(itemView);
             studentClassName = itemView.findViewById(R.id.textStudentClassName);
+            parent = itemView.findViewById(R.id.studentClassList);
         }
     }
 }
--- MP-HW1/app/src/main/java/com/example/quera/StudentDashBoardActivity.java
+++ MP-HW1/app/src/main/java/com/example/quera/StudentDashBoardActivity.java
@@ -1,9 +1,16 @@
 package com.example.quera;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+import com.example.quera.Models.Student;
+
+import java.util.ArrayList;
 
 public class StudentDashBoardActivity extends AppCompatActivity {
 
@@ -15,5 +22,17 @@ public class StudentDashBoardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_student_dash_board);
 
         studentDashBoardRecycleView = findViewById(R.id.studentDashBoardRecycleView);
+
+        ArrayList<StudentClass> studentClasses = new ArrayList<>();
+        studentClasses.add(new StudentClass("Linear Algebra", "Dr", "12"));
+        studentClasses.add(new StudentClass("Mobile Programming", "Dr", "14"));
+        studentClasses.add(new StudentClass("Artificial intelligence", "Dr", "16"));
+
+
+        StudentClassAdapter adapter = new StudentClassAdapter(this);
+        adapter.setStudentClasses(studentClasses);
+
+        studentDashBoardRecycleView.setAdapter(adapter);
+        studentDashBoardRecycleView.setLayoutManager(new GridLayoutManager(this, 2));
     }
 }
